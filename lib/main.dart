7b1a660bbb3ea6cbc7f129cf248fb9f3fb5e73bc
Marcelo-Henrique-dev <dvmarcelohenrique@gmail.com>
2026-04-28 12:50:24 -0300

    splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:nasa_app/features/gallery_page/gallery_page.dart';
+import 'package:nasa_app/features/splash/splash.dart';
 
 void main(){
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: GalleryPage(),
+      home: Splash(),
     ),
   );
 }
